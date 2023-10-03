7d86db200e582ae799019b5b16c7d0ddd2b15c09
“isolcat” <308750414@qq.com>
2023-10-03 19:32:06 +0800

    feat:RSS link validation

--- extension.js
+++ extension.js
@@ -3,12 +3,14 @@ const axios = require("axios");
 const xmlParser = require("fast-xml-parser");
 
 const RSS_LINKS_CONFIG_KEY = 'allblog.rssLinks';
+const RSS_LINK_REGEX = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
 
 async function activate(context) {
 	let rssLinks = vscode.workspace.getConfiguration().get(RSS_LINKS_CONFIG_KEY, {});
 
 	async function parseArticles(link) {
 		const response = await axios.get(link);
+		//解析RSS链接中的文章
 		const articles = xmlParser.parse(response.data).rss.channel.item;
 		return articles;
 	}
@@ -16,6 +18,7 @@ async function activate(context) {
 	async function getArticleContent(link, customName) {
 		const articles = await parseArticles(link);
 
+		// 创建文章列表以供用户选择
 		const articleList = articles.map((article, index) => ({
 			label: article.title,
 			description: article.description,
@@ -30,8 +33,9 @@ async function activate(context) {
 		if (selectedArticle) {
 			const { link } = selectedArticle;
 
+			//自定义高度
 			const heightInput = await vscode.window.showInputBox({
-				prompt: 'Enter the height of the window (in pixels)',
+				prompt: 'Enter the height of the window (in px)',
 				placeHolder: '500'
 			});
 
@@ -67,7 +71,7 @@ async function activate(context) {
 	let disposable = vscode.commands.registerCommand('allblog.searchBlog', async () => {
 		const choiceItems = [
 			{ label: 'Add new RSS link', isNew: true },
-			...Object.keys(rssLinks).map(link => ({ label: rssLinks[link], link: link, isNew: false }))
+			...Object.keys(rssLinks).map(link => ({ label: rssLinks[link], link, isNew: false }))
 		];
 
 		const selectedChoice = await vscode.window.showQuickPick(choiceItems, {
@@ -77,7 +81,13 @@ async function activate(context) {
 		if (selectedChoice) {
 			if (selectedChoice.isNew) {
 				const newLink = await vscode.window.showInputBox({
-					prompt: 'Enter RSS link'
+					prompt: 'Enter RSS link',
+					validateInput: (text) => {
+						if (!RSS_LINK_REGEX.test(text)) {
+							return 'Please enter a valid RSS link (starting with http:// or https://)';
+						}
+						return null;
+					}
 				});
 
 				if (newLink) {
@@ -86,7 +96,7 @@ async function activate(context) {
 					});
 
 					if (customName) {
-						// Check for duplicate custom names
+						// 检查重复的自定义名称
 						const duplicateCustomName = Object.values(rssLinks).includes(customName);
 						if (duplicateCustomName) {
 							vscode.window.showErrorMessage('This custom name is already in use. Please choose a different name.');
@@ -95,6 +105,7 @@ async function activate(context) {
 
 						// 添加新的RSS链接和对应的自定义名字
 						rssLinks[newLink] = customName;
+						// 订阅信息会保存在用户的本地配置中
 						vscode.workspace.getConfiguration().update(RSS_LINKS_CONFIG_KEY, rssLinks, vscode.ConfigurationTarget.Global);
 						await getArticleContent(newLink, customName);
 					}
